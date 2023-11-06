2a9391a8f099d61c6ba6b22c74e1e6fae71b0249
BeamyComet <2013559@leedstrinity.ac.uk>
2023-11-06 21:11:22 +0000

    Added java code for Email/Password Auth in register.java and login.java

--- app/src/main/java/com/example/lab4task/login.java
+++ app/src/main/java/com/example/lab4task/login.java
@@ -1,14 +1,73 @@
 package com.example.lab4task;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    EditText mEmail, mPassword;
+    Button mLoginButton;
+    TextView mCreateButton;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TestUtils.isEmpty(email)){
+                    mEmail.setError("An Email address is required!");
+                    return;
+                }
+
+                // Authenticate User
+                fAuth.signInWithEmailAndPassword(email,
+                        password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(login.this, "Logged in Successfully!",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),
+                                    MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(login.this, "Error!" +
+                                    task.getException(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+
+    });
+        mCreateButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(),
+                register.class));
+        }
+    });
+}}
--- app/src/main/java/com/example/lab4task/register.java
+++ app/src/main/java/com/example/lab4task/register.java
@@ -1,14 +1,59 @@
 package com.example.lab4task;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register extends AppCompatActivity {
-
+    EditText mFLName, mEmail, mPass, mPhone;
+    Button mRegisterButton;
+    TextView mLoginButton;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        progressBar.setVisibility(View.VISIBLE);
+        // Checks if the user is registered
+        if (fAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),
+                    MainActivity.class));
+            finish();
+        }
+        //Registering the user
+
+        String email = "";
+        String password = "";
+        fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(register.this, "User Created",
+                            Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), login.class));
+            }
+        });
     }
 }
